64c621aff51f16ed73878044fd12fc0fa0f6e00c
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-19 21:39:51 +0530

    Update Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Header() {
     return (
         <header className="text-gray-600 body-font" >
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a href="/" className="cursor-pointer flex cursor title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <Link href="/" className="cursor-pointer flex cursor title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <span className="ml-3 text-xl">Frontend Web</span>
-                </a>
+                </Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     <ul className="flex">
 
                         <li className="mr-5 hover:text-gray-900">
-                            <a href='/'>
+                            <Link href='/'>
                                 Home
-                            </a>
+                            </Link>
                         </li>
 
                         <li className="mr-5 hover:text-gray-900">
-                            <a href='/about'>
+                            <Link href='/about'>
                                 About us
-                            </a>
+                            </Link>
                         </li>
 
                     </ul>

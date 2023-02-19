ea3e007e1ceae64a40c4af4d65506e6376c8e33a
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-19 21:40:40 +0530

    Update Post-card component

--- components/Post-card.tsx
+++ components/Post-card.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 function PostCard() {
@@ -14,20 +15,20 @@ function PostCard() {
                             React
                         </h2>
 
-                        <a href='/read' className="block mt-4 text-2xl font-semibold text-gray-800 hover:text-gray-500 dark:text-white md:text-3xl"> Understand The Android Features Before You Regret. </a>
+                        <Link href='/read' className="block mt-4 text-2xl font-semibold text-gray-800 hover:text-gray-500 dark:text-white md:text-3xl"> Understand The Android Features Before You Regret. </Link>
 
                         <p className="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
                         Lorem excepteur dolore ex veniam ad velit officia enim velit consequat consequat nulla eiusmod
                         </p>
 
-                        <a href='/read' className="inline-block mt-2 text-blue-500  hover:text-blue-300">Read more</a>
+                        <Link href='/read' className="inline-block mt-2 text-blue-500  hover:text-blue-300">Read more</Link>
 
                         <div className="flex items-center mt-6">
 
                             <img className="border-none object-cover object-center w-10 h-10 rounded-full" src="https://picsum.photos/200" alt="Bobby Ramos" />
 
                                 <div className="mx-4">
-                                    <a href="/author" className="text-sm text-gray-700 dark:text-gray-200"> Bobby Ramos </a>
+                                    <Link href="/author" className="text-sm text-gray-700 dark:text-gray-200"> Bobby Ramos </Link>
                                     <p className="text-sm text-gray-500 dark:text-gray-400">Technical writer
                                     </p>
                                 </div>

4d3ff730d91c1d1cb9d70fb3b8a0034cb3d3b317
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-20 12:48:59 +0530

    create the contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,43 @@
+import type { ReactElement } from 'react'
+import Layout from '@/components/Layout'
+
+const Contact = () => {
+    return <div className="format dark:format-invert mt-4 mx-auto">
+
+        <h1>Contact us </h1>
+        <p className="lead">Lorem excepteur dolore ex veniam ad velit officia enim velit consequat consequat nulla eiusmod.</p>
+
+
+        <form>
+            <div className="mt-5 mb-6">
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@officialrajdeepsingh.com" required />
+            </div>
+
+            <div className="mb-6">
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter name</label>
+                <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter name" required />.
+            </div>
+            
+            <div className="mb-6">
+                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Write message</label>
+                <textarea id="message" placeholder="Write the message" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            </div>
+
+
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </form>
+
+    </div>
+
+}
+
+Contact.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <Layout>
+            {page}
+        </Layout>
+    )
+}
+
+export default Contact
